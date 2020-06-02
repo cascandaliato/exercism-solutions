d73c2aabca5de2bdabf87f50b18b52c6f760eae5
casca <carmelo.scandaliato@gmail.com>
2020-06-02 14:37:38 +0200

    typescript: collatz conjecture

--- typescript/collatz-conjecture/collatz-conjecture.ts
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -5,16 +5,17 @@ class CollatzConjecture {
     if (n < 1) {
       throw new Error('Only positive numbers are allowed');
     }
-    let sequenceLength = 0;
+
+    let steps = 0;
     while (n !== COLLATZ_SEQUENCE_END) {
-      sequenceLength++;
+      steps++;
       if (n % 2 == 0) {
         n /= 2;
       } else {
         n = 3 * n + 1;
       }
     }
-    return sequenceLength;
+    return steps;
   }
 }
 
