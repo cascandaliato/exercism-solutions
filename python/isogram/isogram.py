bbf8694e3b3fe82fd8fa566b89fc496a872c1f01
casca <carmelo.scandaliato@gmail.com>
2020-06-03 20:34:31 +0200

    python: isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -2,4 +2,4 @@ from collections import Counter
 
 
 def is_isogram(string):
-    return string == "" or Counter([c for c in string.lower() if c.isalnum()]).most_common(1)[0][1] == 1
+    return string == "" or Counter([c for c in string.lower() if c.isalpha()]).most_common(1)[0][1] == 1
