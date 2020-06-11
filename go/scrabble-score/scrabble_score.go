a3495b324e1fb5fb67227c4e8e265d96d2a12ffc
casca <carmelo.scandaliato@gmail.com>
2020-06-11 21:26:11 +0200

    go: scrabble score

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -3,35 +3,40 @@ package scrabble
 
 import "strings"
 
-var letterValues map[rune]uint8 = map[rune]uint8{}
-
-func init() {
-	values := []struct {
-		letters []string
-		value   uint8
-	}{
-		{[]string{"a", "e", "i", "o", "u", "l", "n", "r", "s", "t"}, 1},
-		{[]string{"d", "g"}, 2},
-		{[]string{"b", "c", "m", "p"}, 3},
-		{[]string{"f", "h", "v", "w", "y"}, 4},
-		{[]string{"k"}, 5},
-		{[]string{"j", "x"}, 8},
-		{[]string{"q", "z"}, 10},
-	}
-
-	for _, v := range values {
-		for _, l := range v.letters {
-			letterValues[[]rune(l)[0]] = v.value
-		}
-	}
-
+var letterValues = map[rune]int{
+	'a': 1,
+	'b': 3,
+	'c': 3,
+	'd': 2,
+	'e': 1,
+	'f': 4,
+	'g': 2,
+	'h': 4,
+	'i': 1,
+	'j': 8,
+	'k': 5,
+	'l': 1,
+	'm': 3,
+	'n': 1,
+	'o': 1,
+	'p': 3,
+	'q': 10,
+	'r': 1,
+	's': 1,
+	't': 1,
+	'u': 1,
+	'v': 4,
+	'w': 4,
+	'x': 8,
+	'y': 4,
+	'z': 10,
 }
 
 // Score calculates the Scrabble score of a word.
 func Score(word string) int {
 	score := 0
 	for _, char := range strings.ToLower(word) {
-		score += int(letterValues[char])
+		score += letterValues[char]
 	}
 	return score
 }
