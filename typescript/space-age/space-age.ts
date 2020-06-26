9a80bb927ee9ae925e15ba15bca680564f05fe95
casca <carmelo.scandaliato@gmail.com>
2020-06-26 11:18:08 +0200

    typescript: space age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -16,14 +16,14 @@ class Space {
 
   constructor(public readonly seconds: number) {}
 
-  onEarth = (): number => this.age('Earth');
-  onJupiter = (): number => this.age('Jupiter');
-  onMars = (): number => this.age('Mars');
-  onMercury = (): number => this.age('Mercury');
-  onNeptune = (): number => this.age('Neptune');
-  onSaturn = (): number => this.age('Saturn');
-  onUranus = (): number => this.age('Uranus');
-  onVenus = (): number => this.age('Venus');
+  onEarth = () => this.age('Earth');
+  onJupiter = () => this.age('Jupiter');
+  onMars = () => this.age('Mars');
+  onMercury = () => this.age('Mercury');
+  onNeptune = () => this.age('Neptune');
+  onSaturn = () => this.age('Saturn');
+  onUranus = () => this.age('Uranus');
+  onVenus = () => this.age('Venus');
 
   private age(planet: Planet): number {
     const secondsInYear = Space.earthOrbitalPeriods[planet] * Space.earthYearSeconds;
