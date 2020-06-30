aaca13a18a125ae483f76f26e616f897fc278eb8
casca <carmelo.scandaliato@gmail.com>
2020-06-30 11:23:44 +0200

    typescript: allergies

--- typescript/allergies/allergies.ts
+++ typescript/allergies/allergies.ts
@@ -11,19 +11,17 @@ const allergenToCode = {
 
 type Allergen = keyof typeof allergenToCode;
 
+const allergens = Object.keys(allergenToCode) as Allergen[];
+
 class Allergies {
   constructor(private readonly score: number) {}
 
   allergicTo(allergen: Allergen): boolean {
-    return this.getBit(Math.log2(allergenToCode[allergen])) === 1;
+    return !!(this.score & allergenToCode[allergen]);
   }
 
   list(): Allergen[] {
-    return (Object.keys(allergenToCode) as Allergen[]).filter(this.allergicTo.bind(this));
-  }
-
-  private getBit(position: number): number {
-    return (this.score >> position) & 1;
+    return allergens.filter(this.allergicTo.bind(this));
   }
 }
 
