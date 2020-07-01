e8411033f256c242e66363d47d675d138b821081
casca <carmelo.scandaliato@gmail.com>
2020-07-01 09:49:42 +0200

    typescript: pangram

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,8 +1,13 @@
+const NUMBER_OF_LETTERS_IN_ALPHABET = 26;
 class Pangram {
   constructor(private readonly text: string) {}
 
   isPangram(): boolean {
-    return new Set(this.text.toLowerCase().match(/[a-z]/g)).size >= 26;
+    return this.countUniqueLetters() === NUMBER_OF_LETTERS_IN_ALPHABET;
+  }
+
+  private countUniqueLetters(): number {
+    return new Set(this.text.toLowerCase().match(/[a-z]/g)).size;
   }
 }
 
