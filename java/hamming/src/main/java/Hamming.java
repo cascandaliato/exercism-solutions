77d4105288d9b61f20cec49a84c6ed43f76dae68
casca <8927157+casca@users.noreply.github.com>
2020-07-11 12:34:01 +0200

    java: hamming

---
+++ java/hamming/src/main/java/Hamming.java
@@ -0,0 +1,27 @@
+public class Hamming {
+    private final int distance;
+
+    public Hamming(String leftStrand, String rightStrand) {
+        if (leftStrand.isEmpty() && !rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("left strand must not be empty.");
+        }
+        if (!leftStrand.isEmpty() && rightStrand.isEmpty()) {
+            throw new IllegalArgumentException("right strand must not be empty.");
+        }
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+
+        int distance = 0;
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                distance++;
+            }
+        }
+        this.distance = distance;
+    }
+
+    public int getHammingDistance() {
+        return this.distance;
+    }
+}
