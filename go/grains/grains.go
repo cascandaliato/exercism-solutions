9c89e27620adcfa6d222c5fd62fa8c6f45b2661d
casca <8927157+casca@users.noreply.github.com>
2020-07-15 14:40:16 +0200

    go: grains

---
+++ go/grains/grains.go
@@ -0,0 +1,16 @@
+package grains
+
+import "errors"
+
+// Square returns the number of grains on the n-th square of a chess board, assuming that the first square holds one grain and the number of grains doubles on each successive square.
+func Square(sq int) (uint64, error) {
+	if sq <= 0 || sq > 64 {
+		return 0, errors.New("The square number must be between 1 and 64")
+	}
+	return 1 << (sq - 1), nil
+}
+
+// Total returns the total number of grains on a chess board, assuming that the first square holds one grain and the number of grains doubles on each successive square.
+func Total() uint64 {
+	return 1<<64 - 1
+}
