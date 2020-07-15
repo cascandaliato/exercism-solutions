7e3102d6f2dbd37db81600434693617c7714f95b
casca <8927157+casca@users.noreply.github.com>
2020-07-15 13:02:00 +0200

    java: raindrops

---
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -0,0 +1,18 @@
+class RaindropConverter {
+
+  String convert(int number) {
+    StringBuilder sound = new StringBuilder();
+
+    if (number % 3 == 0) {
+      sound.append("Pling");
+    }
+    if (number % 5 == 0) {
+      sound.append("Plang");
+    }
+    if (number % 7 == 0) {
+      sound.append("Plong");
+    }
+
+    return sound.length() > 0 ? sound.toString() : String.valueOf(number);
+  }
+}
