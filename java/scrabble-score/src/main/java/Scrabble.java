2bf3642490ba0e473509bb60eb633767cc993b1e
casca <8927157+casca@users.noreply.github.com>
2020-07-24 12:51:02 +0200

    java: scrabble score

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,44 +1,18 @@
-import java.util.HashMap;
 import java.util.Map;
+import static java.util.Map.entry;
 
-@SuppressWarnings("serial")
 class Scrabble {
 
-    private final Map<Character, Integer> letterValues = new HashMap<Character, Integer>() {
-        {
-            put('a', 1);
-            put('b', 3);
-            put('c', 3);
-            put('d', 2);
-            put('e', 1);
-            put('f', 4);
-            put('g', 2);
-            put('h', 4);
-            put('i', 1);
-            put('j', 8);
-            put('k', 5);
-            put('l', 1);
-            put('m', 3);
-            put('n', 1);
-            put('o', 1);
-            put('p', 3);
-            put('q', 10);
-            put('r', 1);
-            put('s', 1);
-            put('t', 1);
-            put('u', 1);
-            put('v', 4);
-            put('w', 4);
-            put('x', 8);
-            put('y', 4);
-            put('z', 10);
-        }
-    };
+    private final Map<Character, Integer> letterValues = Map.ofEntries(entry('a', 1), entry('b', 3), entry('c', 3),
+            entry('d', 2), entry('e', 1), entry('f', 4), entry('g', 2), entry('h', 4), entry('i', 1), entry('j', 8),
+            entry('k', 5), entry('l', 1), entry('m', 3), entry('n', 1), entry('o', 1), entry('p', 3), entry('q', 10),
+            entry('r', 1), entry('s', 1), entry('t', 1), entry('u', 1), entry('v', 4), entry('w', 4), entry('x', 8),
+            entry('y', 4), entry('z', 10));
 
     private int score;
 
     Scrabble(String word) {
-        this.score = word.toLowerCase().chars().map((c) -> letterValues.get((char) c)).reduce(0, Integer::sum);
+        this.score = word.toLowerCase().chars().map(c -> letterValues.get((char) c)).reduce(0, Integer::sum);
     }
 
     int getScore() {
