a7b24eaef6383b521798f161315ccc108397631c
casca <8927157+casca@users.noreply.github.com>
2020-08-09 09:03:58 +0200

    go: nucleotice count

--- go/nucleotide-count/nucleotide_count.go
+++ go/nucleotide-count/nucleotide_count.go
@@ -9,19 +9,12 @@ type Histogram map[rune]uint32
 // DNA is a sequence of nucleotides.
 type DNA string
 
-var nucleotides = map[rune]bool{
-	'A': true,
-	'C': true,
-	'G': true,
-	'T': true,
-}
-
 // Counts generates a histogram of valid nucleotides in the given DNA.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram = map[rune]uint32{'A': 0, 'C': 0, 'G': 0, 'T': 0}
+	h := Histogram{'A': 0, 'C': 0, 'G': 0, 'T': 0}
 
 	for _, n := range d {
-		if _, ok := nucleotides[n]; !ok {
+		if _, ok := h[n]; !ok {
 			return h, fmt.Errorf("DNA strand %s contains a non-valid nucleotide %c", d, n)
 		}
 		h[n]++
