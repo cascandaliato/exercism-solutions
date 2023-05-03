64f3a3383c14f97f46deafe5b9808b2fc5de6d71
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-03 15:12:46 +0100

    Rename variables in go/etl

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -5,10 +5,10 @@ import "strings"
 
 // Transform transforms Scrabble scores from a legacy "list of letters per score" format to the shiny new "score per letter" format.
 func Transform(legacy map[int][]string) map[string]int {
-	scorePerLetter := map[string]int{}
-	for s, ll := range legacy {
-		for _, l := range ll {
-			scorePerLetter[strings.ToLower(l)] = s
+	scorePerLetter := make(map[string]int)
+	for score, letters := range legacy {
+		for _, letter := range letters {
+			scorePerLetter[strings.ToLower(letter)] = score
 		}
 	}
 	return scorePerLetter
