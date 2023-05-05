791255377ca742cb4fca72fc75e2521058da702b
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-05 13:04:25 +0100

    Refactor go/reverse-string

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -4,9 +4,9 @@ package reverse
 // Reverse reverses the input string.
 func Reverse(input string) string {
 	runes := []rune(input)
-	reversed := make([]rune, len(runes))
-	for i := 0; i < len(runes); i++ {
-		reversed[len(runes)-1-i] = runes[i]
+	for i := 0; i < len(runes)/2; i++ {
+		j := len(runes) - 1 - i
+		runes[i], runes[j] = runes[j], runes[i]
 	}
-	return string(reversed)
+	return string(runes)
 }
