7ac445f96fe85f3045accf205622e29ca3591c1a
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-05 13:04:38 +0100

    Refactor go/roman-numerals

--- go/roman-numerals/roman_numerals.go
+++ go/roman-numerals/roman_numerals.go
@@ -4,9 +4,10 @@ package romannumerals
 import (
 	"errors"
 	"strconv"
+	"strings"
 )
 
-var letters []rune = []rune{'I', 'V', 'X', 'L', 'C', 'D', 'M'}
+var letters []byte = []byte{'I', 'V', 'X', 'L', 'C', 'D', 'M'}
 
 // ToRomanNumeral converts any positive number up to 3,999 into a Roman numeral.
 func ToRomanNumeral(arabic int) (string, error) {
@@ -14,33 +15,30 @@ func ToRomanNumeral(arabic int) (string, error) {
 		return "", errors.New("cannot convert numbers <= 0 or > 3999")
 	}
 
-	roman := make([]rune, 0)
-	digits := []rune(strconv.Itoa(arabic))
+	var numeral strings.Builder
+	digits := []byte(strconv.Itoa(arabic))
 	for i, j := 0, 2*(len(digits)-1); i < len(digits); i, j = i+1, j-2 {
-		var runes []rune
 		switch digits[i] {
 		case '0':
 		case '1':
-			runes = []rune{letters[j]}
+			numeral.WriteByte(letters[j])
 		case '2':
-			runes = []rune{letters[j], letters[j]}
+			numeral.Write([]byte{letters[j], letters[j]})
 		case '3':
-			runes = []rune{letters[j], letters[j], letters[j]}
+			numeral.Write([]byte{letters[j], letters[j], letters[j]})
 		case '4':
-			runes = []rune{letters[j], letters[j+1]}
+			numeral.Write([]byte{letters[j], letters[j+1]})
 		case '5':
-			runes = []rune{letters[j+1]}
+			numeral.WriteByte(letters[j+1])
 		case '6':
-			runes = []rune{letters[j+1], letters[j]}
+			numeral.Write([]byte{letters[j+1], letters[j]})
 		case '7':
-			runes = []rune{letters[j+1], letters[j], letters[j]}
+			numeral.Write([]byte{letters[j+1], letters[j], letters[j]})
 		case '8':
-			runes = []rune{letters[j+1], letters[j], letters[j], letters[j]}
+			numeral.Write([]byte{letters[j+1], letters[j], letters[j], letters[j]})
 		case '9':
-			runes = []rune{letters[j], letters[j+2]}
+			numeral.Write([]byte{letters[j], letters[j+2]})
 		}
-		roman = append(roman, runes...)
 	}
-
-	return string(roman), nil
+	return numeral.String(), nil
 }
