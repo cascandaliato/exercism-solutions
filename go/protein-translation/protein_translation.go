0ea430323b3cddb755bf11600da37b325e2f5d10
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-06 11:21:52 +0200

    Refactor go/protein-translation

--- go/protein-translation/protein_translation.go
+++ go/protein-translation/protein_translation.go
@@ -12,45 +12,54 @@ const (
 	tyrosine      = "Tyrosine"
 	cysteine      = "Cysteine"
 	tryptophan    = "Tryptophan"
-	stop          = "STOP"
 )
 
-var codonToProtein = map[string]string{
-	"AUG": methionine,
-	"UUU": phenylalanine,
-	"UUC": phenylalanine,
-	"UUA": leucine,
-	"UUG": leucine,
-	"UCU": serine,
-	"UCC": serine,
-	"UCA": serine,
-	"UCG": serine,
-	"UAU": tyrosine,
-	"UAC": tyrosine,
-	"UGU": cysteine,
-	"UGC": cysteine,
-	"UGG": tryptophan,
-	"UAA": stop,
-	"UAG": stop,
-	"UGA": stop,
-}
-
-// ErrStop happens when trying to convert a 'STOP' codon into a protein.
-var ErrStop = errors.New("cannot convert a 'STOP' codon into a protein")
+// ErrStop happens when trying to convert a 'STOP' codon into an amino acid.
+var ErrStop = errors.New("cannot convert a 'STOP' codon into an amino acid")
 
 // ErrInvalidBase happens when an invalid codon value is specified.
-var ErrInvalidBase = errors.New("cannot convert an invalid codon into a protein")
+var ErrInvalidBase = errors.New("cannot convert an invalid codon into an amino acid")
 
-// FromCodon converts a codon into the corresponding protein.
+// FromCodon converts a codon into the corresponding amino acid.
 func FromCodon(codon string) (string, error) {
-	protein, ok := codonToProtein[codon]
-	if !ok {
+	switch codon {
+	case "AUG":
+		return methionine, nil
+	case "UUU":
+		return phenylalanine, nil
+	case "UUC":
+		return phenylalanine, nil
+	case "UUA":
+		return leucine, nil
+	case "UUG":
+		return leucine, nil
+	case "UCU":
+		return serine, nil
+	case "UCC":
+		return serine, nil
+	case "UCA":
+		return serine, nil
+	case "UCG":
+		return serine, nil
+	case "UAU":
+		return tyrosine, nil
+	case "UAC":
+		return tyrosine, nil
+	case "UGU":
+		return cysteine, nil
+	case "UGC":
+		return cysteine, nil
+	case "UGG":
+		return tryptophan, nil
+	case "UAA":
+		return "", ErrStop
+	case "UAG":
+		return "", ErrStop
+	case "UGA":
+		return "", ErrStop
+	default:
 		return "", ErrInvalidBase
 	}
-	if protein == stop {
-		return "", ErrStop
-	}
-	return protein, nil
 }
 
 // FromRNA converts an RNA sequence into a sequence of proteins.
@@ -62,7 +71,7 @@ func FromRNA(rna string) ([]string, error) {
 			if err == ErrStop {
 				break
 			}
-			return proteins, err
+			return nil, err
 		}
 		proteins = append(proteins, protein)
 	}
