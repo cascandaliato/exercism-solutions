8d1d567be492e2df4e41c1ebdeb978ad85f4ec09
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-16 08:53:16 +0200

    go/gigasecond: move gigasecond to constant

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -2,11 +2,12 @@
 package gigasecond
 
 import (
-	"math"
 	"time"
 )
 
+const gigasecond = 1e9 * time.Second
+
 // AddGigasecond returns a new time set one gigasecond (~30 years) in the future of the provided time.
 func AddGigasecond(t time.Time) time.Time {
-	return t.Add(time.Duration(math.Pow(10, 9)) * time.Second)
+	return t.Add(gigasecond)
 }
