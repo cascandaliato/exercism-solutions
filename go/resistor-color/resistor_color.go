6cf07cccf860ecd2b9bb13fa43843090308f10c2
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-17 08:31:15 +0200

    Solve go/resistor-color

---
+++ go/resistor-color/resistor_color.go
@@ -0,0 +1,28 @@
+package resistorcolor
+
+var colorCodes map[string]int = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
+// Colors should return the list of all colors.
+func Colors() []string {
+	colors := make([]string, 0, len(colorCodes))
+	for color := range colorCodes {
+		colors = append(colors, color)
+	}
+	return colors
+}
+
+// ColorCode returns the resistance value of the given color.
+func ColorCode(color string) int {
+	return colorCodes[color]
+}
