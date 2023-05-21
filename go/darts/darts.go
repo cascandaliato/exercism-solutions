ab0d5277d2475dde37606a8f315e2ccf19a17d72
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-21 08:21:17 +0200

    Refactor go/darts

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -2,13 +2,19 @@ package darts
 
 import "math"
 
+const (
+	inner  = 1
+	middle = 5
+	outer  = 10
+)
+
 func Score(x, y float64) int {
-	switch r := math.Sqrt(x*x + y*y); {
-	case r <= 1:
+	switch distance := math.Sqrt(x*x + y*y); {
+	case distance <= inner:
 		return 10
-	case r <= 5:
+	case distance <= middle:
 		return 5
-	case r <= 10:
+	case distance <= outer:
 		return 1
 	default:
 		return 0
