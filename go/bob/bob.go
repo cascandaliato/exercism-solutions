dbace390ddbc06d8b20ba688af65dcdee554b425
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2023-05-23 08:14:22 +0200

    Solve go/bob

---
+++ go/bob/bob.go
@@ -0,0 +1,51 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package bob should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package bob
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+// Hey should have a comment documenting it.
+func Hey(remark string) string {
+	remark = strings.TrimSpace(remark)
+	switch {
+	case isQuestion(remark) && isUpper(remark):
+		return "Calm down, I know what I'm doing!"
+	case isQuestion(remark):
+		return "Sure."
+	case isUpper(remark):
+		return "Whoa, chill out!"
+	case remark == "":
+		return "Fine. Be that way!"
+	default:
+		return "Whatever."
+	}
+}
+
+func isQuestion(remark string) bool {
+	var last rune
+	for _, r := range remark {
+		last = r
+	}
+	return last == '?'
+}
+
+func isUpper(remark string) bool {
+	containsLetter := false
+	for _, r := range remark {
+		fmt.Println(r, unicode.IsLetter(r), !unicode.IsUpper(r))
+		if unicode.IsLetter(r) {
+			containsLetter = true
+			if !unicode.IsUpper(r) {
+				return false
+			}
+		}
+	}
+	return containsLetter
+}
